edd4d4fed191a31a655313263510097d3d9c8cde
kvaldesvallejo <kvaldesvallejo@gmail.com>
2020-12-17 05:14:17 +0000

    Only a few exercises are started and not ready yet

--- exercises/14-Todo-List/index.js
+++ exercises/14-Todo-List/index.js
@@ -1,1 +1,26 @@
 // Your code here
+let input = document.querySelector("#addToDo");
+input.addEventListener("keyup", addTodo);
+
+let ul = document.querySelector("ul");
+let li = document.querySelectorAll("li");
+li.addEventListener("click", removeTodo);
+//for (let l of li) {
+//	l.addEventListener("click", removeTodo);
+//}
+
+function addTodo(event) {
+	if (event.keyCode == 13) {
+		let task = event.target.value;
+		ul.innerHTML += `<li>
+					<span><i class="fa fa-trash"></i></span> ${task}
+				</li>`;
+
+		console.log(event);
+	}
+}
+function removeTodo(event) {
+    console.log(event);
+    alert("Hello World");
+	//li.parentNode.removeChild(event);
+}
